962e0a90ad4a431915a7c5069fa0a07a22db9d63
Lukasz Duda <lukasz.duda@nordicsemi.no>
2021-04-09 00:27:23 +0200

    Task 5 & 6: Threads + SW PWM

--- src/main.c
+++ src/main.c
@@ -13,35 +13,36 @@
 #define STATUS_LED_INTERVAL 500
 #define APP_LED             DK_LED4
 
+#define LED_THREAD_STACK_SIZE 500
+#define LED_THREAD_PRIORITY   5
+
+#define PWM_THREAD_STACK_SIZE 500
+#define PWM_THREAD_PRIORITY   5
+
+#define PWM_LEVEL_MAX (10-1)
+
 LOG_MODULE_REGISTER(APP);
 
-static void led_timer_handler(struct k_timer *timer_id);
-K_TIMER_DEFINE(led_timer, led_timer_handler, NULL);
+static volatile int level = 0;
 
 static void button_handler(uint32_t button_state, uint32_t has_changed)
 {
-	int err;
-
 	uint32_t button_mask = button_state & has_changed;
 
 	if (button_mask & DK_BTN1_MSK)
 	{
 		LOG_INF("Button 1 has been pushed.");
 
-		err = dk_set_led_off(APP_LED);
-		if (err) {
-			LOG_ERR("Failed to turn off the LED. Error %d\n", err);
-		}
+		if (level > 0)
+			level--;
 	}
 
 	if (button_mask & DK_BTN2_MSK)
 	{
 		LOG_INF("Button 2 has been pushed.");
 
-		err = dk_set_led_on(APP_LED);
-		if (err) {
-			LOG_ERR("Failed to turn on the LED. Error %d\n", err);
-		}
+		if (level < PWM_LEVEL_MAX)
+			level++;
 	}
 }
 
@@ -61,13 +62,35 @@ static void init_buttons(void)
 	}
 }
 
-static void led_timer_handler(struct k_timer *timer_id)
+static void led_thread(void)
 {
-	static int cnt = 0;
+	int cnt = 0;
 
-	int err = dk_set_led(STATUS_LED, cnt++%2);
-	if (err) {
-		LOG_ERR("Failed to set the LED. Error %d\n", err);
+	while (1) {
+		int err = dk_set_led(STATUS_LED, cnt++%2);
+		if (err) {
+			LOG_ERR("Failed to set the LED. Error %d\n", err);
+		}
+
+		k_msleep(STATUS_LED_INTERVAL);
+	}
+}
+
+static void pwm_thread(void)
+{
+	while(1) {
+		if (level) {
+			int active_time_ms   = level;
+			int inactive_time_ms = PWM_LEVEL_MAX - active_time_ms;
+
+			dk_set_led_on(APP_LED);
+			k_msleep(active_time_ms);
+			dk_set_led_off(APP_LED);
+			k_msleep(inactive_time_ms);
+		}
+		else {
+			k_msleep(100);
+		}
 	}
 }
 
@@ -78,7 +101,12 @@ void main(void)
 
 	init_leds();
 	init_buttons();
-
-	// Start status LED timer.
-	k_timer_start(&led_timer, K_MSEC(STATUS_LED_INTERVAL), K_MSEC(STATUS_LED_INTERVAL));
 }
+
+K_THREAD_DEFINE(led_tid, LED_THREAD_STACK_SIZE,
+                (k_thread_entry_t)led_thread, NULL, NULL, NULL,
+                LED_THREAD_PRIORITY, 0, 0);
+
+K_THREAD_DEFINE(pwm_tid, PWM_THREAD_STACK_SIZE,
+                (k_thread_entry_t)pwm_thread, NULL, NULL, NULL,
+                PWM_THREAD_PRIORITY, 0, 0);
