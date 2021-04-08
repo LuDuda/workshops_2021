cd841d82df56b4e5cda96e4cead0e1ff77981159
Lukasz Duda <lukasz.duda@nordicsemi.no>
2021-04-08 23:34:18 +0200

    Task 4: Control buttons

--- src/main.c
+++ src/main.c
@@ -11,17 +11,53 @@
 
 #define STATUS_LED          DK_LED3
 #define STATUS_LED_INTERVAL 500
+#define APP_LED             DK_LED4
 
 LOG_MODULE_REGISTER(APP);
 
 static void led_timer_handler(struct k_timer *timer_id);
 K_TIMER_DEFINE(led_timer, led_timer_handler, NULL);
 
+static void button_handler(uint32_t button_state, uint32_t has_changed)
+{
+	int err;
+
+	uint32_t button_mask = button_state & has_changed;
+
+	if (button_mask & DK_BTN1_MSK)
+	{
+		LOG_INF("Button 1 has been pushed.");
+
+		err = dk_set_led_off(APP_LED);
+		if (err) {
+			LOG_ERR("Failed to turn off the LED. Error %d\n", err);
+		}
+	}
+
+	if (button_mask & DK_BTN2_MSK)
+	{
+		LOG_INF("Button 2 has been pushed.");
+
+		err = dk_set_led_on(APP_LED);
+		if (err) {
+			LOG_ERR("Failed to turn on the LED. Error %d\n", err);
+		}
+	}
+}
+
 static void init_leds(void)
 {
 	int err = dk_leds_init();
 	if (err) {
 		LOG_ERR("Failed to initialize LEDs module. Error %d\n", err);
+	}
+}
+
+static void init_buttons(void)
+{
+	int err = dk_buttons_init(button_handler);
+	if (err) {
+		LOG_ERR("Failed to initialize buttons module. Error %d\n", err);
 	}
 }
 
@@ -41,6 +77,7 @@ void main(void)
 	LOG_DBG("Board: %s\n", CONFIG_BOARD);
 
 	init_leds();
+	init_buttons();
 
 	// Start status LED timer.
 	k_timer_start(&led_timer, K_MSEC(STATUS_LED_INTERVAL), K_MSEC(STATUS_LED_INTERVAL));
